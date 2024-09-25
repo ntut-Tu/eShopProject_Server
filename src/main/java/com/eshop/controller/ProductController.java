d3f07f61a213ad19073ebcd10f61f40e1126faa6
quinndu0 <160988691+ntut-Tu@users.noreply.github.com>
2024-09-25 22:38:09 +0800

    Upload Project

---
+++ src/main/java/com/eshop/controller/ProductController.java
@@ -0,0 +1,36 @@
+package com.eshop.controller;
+
+import com.eshop.model.Product;
+import com.eshop.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductController {
+    @Autowired
+    private ProductService productService;
+
+    @GetMapping("/work_status")
+    public String getWorkStatus() {
+        return "Server working!";
+    }
+
+    @GetMapping("")
+    public List<Product> getAllProducts() {
+        return productService.getAllProducts();
+    }
+
+    @GetMapping("/{id}")
+    public Product getProduct(@PathVariable Long id) {
+        return productService.getProductById(id);
+    }
+
+    @PostMapping("/")
+    public Product createProduct(@RequestBody Product product) {
+        return productService.createProduct(product);
+    }
+
+}
---
+++ src/main/java/com/eshop/service/ProductService.java
@@ -0,0 +1,27 @@
+package com.eshop.service;
+
+import com.eshop.model.Product;
+import com.eshop.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductService {
+    @Autowired
+    private ProductRepository productRepository;
+
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+
+    public Product getProductById(Long id) {
+        return productRepository.findById(id).orElse(null);
+    }
+
+    public Product createProduct(Product product) {
+        return productRepository.save(product);
+    }
+
+}
